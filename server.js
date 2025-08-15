49f1561a67ce186c53b795ddcce1f90c70cc6b2e
IT21132650 <it21132650@my.sliit.lk>
2025-08-15 12:54:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port =3001;
+const host = 'localhost';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://buddhikaeranga54:9563@cluster1.cmszlan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1';
+
+const connect = async () => {
+    try{
+        await mongoose.connect(uri);
+        console.log('Connected to mongoDB');
+    }
+    catch(error){
+        console.log('MongoDB Error: ',error);
+    }
+};
+
+connect();
+
+const server = app.listen(3001, '127.0.0.1', () => {
+    console.log(`Node server is listening to ${server.address().port}`)
+});
+
+app.use('/api', router);

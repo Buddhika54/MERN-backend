779ee7589925d6be9621bb0b597bef3418468cec
Buddhika54 <buddhikaeranga54@gmail.com>
2025-09-06 14:07:09 +0530

    first commit

--- app.js
+++ app.js
@@ -13,28 +13,48 @@ app.use(
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    controller.getUsers((req, res, next) => {
-        res.send();
-    });
 
-});
-
-app.post('/createuser', (req, res) => {
-    controller.addUser(req.body, (callack) => {
+app.post('/admin', (req, res) =>{
+    controller.adminRegister((req, res, next) =>{
         res.send();
     });
 });
 
-app.post('/updateuser', (req, res) => {
-    controller.updateUser(req.body, (callack) => {
-        res.send(callack);
+app.post('/adlogin', (req, res) =>{
+    controller.adminLogin((req, res, next) =>{
+        res.send();
     });
 });
 
-app.post('/deleteuser', (req, res) => {
-    controller.deleteUser(req.body, (callack) => {
-        res.send(callack);
+app.get('/verify', (req, res) =>{
+    controller.verify((req, res) =>{
+        res.send();
+    });
+});
+
+
+
+app.post('/add', (req,res) => {
+    controller.addDepartment((req, res) => {
+        res.send();
+    });
+});
+
+app.post('/empadd', (req,res) => {
+    controller.addEmployee((req, res) => {
+        res.send();
+    });
+});
+
+app.get('/department', (req, res) => {
+    controller.getDepartment((req,res) => {
+        res.send();
+    });
+});
+
+app.get('/employee', (req, res) => {
+    controller.getEmployee((req,res) => {
+        res.send();
     });
 });
 
--- controller.js
+++ controller.js
@@ -1,57 +1,212 @@
 const { response } = require('./app');
-const User = require('./model');
+const Admin= require('./model/model');
+const Department = require('./model/AddDepartment');
+const Employee = require('./model/Emplyee');
+const bcrypt = require('bcrypt');
+const jwt = require ('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
 
 
-const getUsers = (req, res, next) => {
-    User.find()
-        .then(response => {
-            res.json({ response })
-        })
-        .catch(error => {
-            res.json({ error })
-        }
-        );
+
+
+
+const adminRegister = async (req, res, next) => {
+
+    try {
+        const hashPassword = await  bcrypt.hash("admin", 10)
+        const admin = new Admin({
+            name: "Admin",
+            email: "admin@gmail.com",
+            password: hashPassword,
+            role: "admin"
+
+    });
+    await admin.save();
+    } catch (error) {
+        console.log(error)
+    }
+    
 };
 
-const addUser = (req, res, next) => {
-    const user = new User({
-        id: req.body.id,
-        name: req.body.name,
-    });
-    user.save()
-        .then(response => {
-            res.json({ response })
-        })
-        .catch(error => {
-            res.json({ error })
+
+const adminLogin = async (req, res, next) => {
+    try{
+        const { email, password } = req.body;
+        const admin = await Admin.findOne({ email })
+        if(!admin){
+            return res.status(404).json({success: false, error: "User Not Found"})
         }
-        );
+
+        const isMatch = await bcrypt.compare(password, admin.password)
+        if(!isMatch){
+            return res.status(404).json({success: false, error: "Wrong Password"})
+        }
+
+        const token = jwt.sign({_id: admin._id, role: admin.role},
+            "jwtScreteKeyAAA333@@@###8889999jjdd",{expiresIn: "10d"}
+        )
+
+        res.status(200).json({success: true, token, admin: {_id: admin._id, name: admin.name, role: admin.role},
+        });
+    }catch(error){
+        return res.status(500).json({success: false, error: error.message});
+    }
+};
+
+const verify = (req,res) => {
+    return res.status(200).json({success: true, admin: req.admin})
 }
 
-const updateUser = (req, res, next) => {
-    const { id,name } = req.body;
-    User.updateOne({ id: id }, { $set: { name: name}})
-        .then(response => {
-            res.json({ response })
+
+const addDepartment = async (req, res) => {
+    try {
+        const {dep_name, description} = req.body;
+        const newDep = new Department({
+            dep_name: dep_name,
+            description: description
         })
-        .catch(error => {
-            res.json({ error })
-        }
-        );
+        await newDep.save()
+        return res.status(200).json({success: true, department: newDep})
+
+    } catch (error) {
+        return res.status(500).json({success: false, error: "add department server error"})
+    }
 }
 
-const deleteUser = (req, res, next) => {
-    const id = req.body.id;
-    User.deleteOne({ id: id})
-        .then(response => {
-            res.json({ response })
-        })
-        .catch(error => {
-            res.json({ error })
-        }
-        );
+const getDepartment = async (req, res) =>{
+    try {
+        const departments = await Department.find()
+        return res.status(200).json({success: true, departments})
+    } catch (error) {
+        return res.status(500).json({success: false, error: "get department server error"})
+    }
 }
-exports.getUsers = getUsers;
-exports.addUser = addUser;
-exports.updateUser = updateUser;
-exports.deleteUser = deleteUser;
+
+const showDepartment = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const department = await Department.findById({_id: id})
+        return res.status(200).json({success: true, department})
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "get show department server error"})
+    }
+}
+
+const updateDepartment = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {dep_name, description} = req.body
+        const updateDep = await Department.findByIdAndUpdate({_id: id}, {
+            dep_name: dep_name,
+            description: description
+        })
+        return res.status(200).json({success: true, updateDep})
+    } catch (error) {
+        return res.status(500).json({success: false, error: "edit department server error"})
+    }
+
+}
+
+const deleteDepartment = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const deleteDep = await Department.findByIdAndDelete({_id: id})
+        return res.status(200).json({success: true, deleteDep})
+    } catch (error) {
+        return res.status(500).json({success: false, error: "delete department server error"})
+    }
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "public/uploads")
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage: storage})
+
+
+const addEmployee = async (req, res) => {
+    try{
+        const{
+            name,
+            email,
+            employeeId,
+            dob,
+            gender,
+            martalStatus,
+            designation,
+            department,
+            salary,
+            password,
+            role,
+
+        }= req.body;
+
+
+        const user = await Admin.findOne({email})
+        if(user){
+            return res.status(400).json({success: false, error: "User already registerd in employee"})
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new Admin({
+            name,
+            email,
+            password: hashPassword,
+            role,
+            profileImage: req.file ? req.file.filename : ""
+        })
+
+        const savedUser = await newUser.save()
+
+        const newEmployee = new Employee({
+            userId: savedUser._id,
+            employeeId,
+            dob,
+            gender,
+            martalStatus,
+            designation,
+            department,
+            salary
+        })
+
+        await newEmployee.save()
+        return res.status(200).json({success: true, message: "employee created"})
+
+    } catch(error){
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "Server error in adding employee"})
+    }
+}
+
+
+const getEmployee = async (req, res) =>{
+    try {
+        
+        const employees = await Employee.find().populate('userId', {password: 0}).populate('department')
+        return res.status(200).json({success: true, employees})
+    } catch (error) {
+        return res.status(500).json({success: false, error: "get employee server error"})
+    }
+}
+
+
+exports.upload = upload.single('image');
+exports.addEmployee = addEmployee;
+exports.deleteDepartment = deleteDepartment;
+exports.updateDepartment = updateDepartment;
+exports.showDepartment = showDepartment;
+exports.getDepartment = getDepartment;
+exports.addDepartment = addDepartment;
+exports.adminRegister = adminRegister;
+exports.adminLogin = adminLogin;
+exports.verify = verify;
+exports.getEmployee = getEmployee;
